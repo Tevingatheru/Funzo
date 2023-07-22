7e5d30c9aca1d9c4d29a3abbed7797ff17a3d431
Mwandati85 <107621378+Mwandati85@users.noreply.github.com>
2023-07-22 19:12:27 +0300

    Login

---
+++ app/src/main/java/com/learner/funzo/Login.kt
@@ -0,0 +1,75 @@
+package com.learner.funzo
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    var editTextEmail: TextInputEditText? = null
+    var editTextPassword: TextInputEditText? = null
+    var buttonLogin: Button? = null
+    var mAuth: FirebaseAuth? = null
+    var textView: TextView? = null
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+//        if (currentUser != null) {
+//            val intent = Intent(applicationContext, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonLogin = findViewById(R.id.btn_login)
+        textView = findViewById(R.id.registerNow)
+        textView!!.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, Register::class.java)
+            startActivity(intent)
+            finish()
+        })
+        buttonLogin!!.setOnClickListener(View.OnClickListener {
+            val email: String
+            val password: String
+            email = editTextEmail!!.getText().toString()
+            password = editTextEmail!!.getText().toString()
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this@Login, "Enter Email", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this@Login, "Enter password", Toast.LENGTH_SHORT).show()
+            }
+            mAuth!!.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this@Login, "Login Successful.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@Login, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        })
+    }
+}
